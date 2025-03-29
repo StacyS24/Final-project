9df0311154f8f51f4b4081fff48d644e334bd074
StacyS24 <stacysouza24@gmail.com>
2025-03-29 16:24:41 +0000

    Update views.py

--- volunteers/views.py
+++ volunteers/views.py
@@ -26,7 +26,7 @@ import requests
 from dotenv import load_dotenv
 from .sample_data import sample_data
 # Create your views here.
-sample_data()
+
 def signup(request):
     """
     This view handles the signup process for any user.
